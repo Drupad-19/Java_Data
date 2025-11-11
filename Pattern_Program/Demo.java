0976f847917615926d09907c40a3261d6f27d235
Drupad-19 <drupadhy07@gmail.com>
2025-11-11 23:49:33 +0530

    Add files via upload

---
+++ Pattern_Program/Demo.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+class Mode{
+   void pattern(){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the n"+" "+ "value");
+int n=s.nextInt();
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+s.close();
+}
+}
+class Demo2 {
+public static void main(String [] args){
+  Mode m=new Mode();
+  m.pattern();
+}
+}
